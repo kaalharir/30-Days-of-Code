80cd68c430360750f54ce75ac9b34b1fa42bf711
kaal <kaalharir@iut-dhaka.edu>
2024-03-29 23:34:03 +0300

    Day 9 and Day 11

---
+++ Day 11.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+int main() {
+	vector<vector<int>> arr(6);
+	for ( int i = 0; i < 6; i++) {
+		arr[i].resize(6);
+		string arr_row_temp_temp;
+		getline(cin, arr_row_temp_temp);
+		vector<string> arr_row_temp_temp = split(rtrim(arr_row_temp_temp));
+		for(int j = 0; j < 6; j++){
+			int arr_row_item = stoi(arr_row_item[j]);
+			arr[i][j] = arr_row_item;
+		}
+	}
+	// Initialize max_sum to the smallest possible value
+	int max_sum = INT_MIN;
+	for(int i = 0; i < 4; i++) {
+		for( int j = 0; j < 4; j++){
+			// Sum of top 3 elements
+			int top = arr[i][j] + arr[i][j+1] + arr[i][j+2];
+			// Middle element
+			int middle = arr[i+1][j+1];
+			// Sum of bottom 3 elements
+			int bottom = arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+			// Total hourglass sum
+			int hourglass = top + middle + bottom;
+			// Update max_sum if this hourglass has the highest sum so
+			if (hourglass > max_sum) {
+				max_sum = hourglass;
+			}
+		}
+	}
+	cout << max_sum << endl;
+	return 0;
+
+}
+
+string ltrim(const string &str) {
+    string s(str);
+    s.erase(s.begin(), find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+    s.erase(find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(), s.end());
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+    string::size_type start = 0;
+    string::size_type end = 0;
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+        start = end + 1;
+    }
+    tokens.push_back(str.substr(start));
+    return tokens;
+}
